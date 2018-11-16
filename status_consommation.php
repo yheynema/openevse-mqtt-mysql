ce177f6fe956d809219c9c1a3310cade2fa49b9e
yheynema <yheynema@gmail.com>
2018-11-15 21:11:19 -0500

    Add files via upload
    Added a summary table, per day

--- status_consommation.php
+++ status_consommation.php
@@ -12,9 +12,9 @@ if ($conn->connect_error) {
 } 
 $unitprice=0.103;
 
-$sql = "select id, from_unixtime(tstamp) as 'DateHeure', value as 'EWs', round(value/3600,1) as 'EWh', round(value/3600/1000*".$unitprice.",3) as 'totdol', round(sdur/60,1) as 'sdurmn', carid, status from energySession order by id desc";
+$sql = "select id, from_unixtime(tstamp) as 'DateHeure', value as 'EWs', round(value/3600,1) as 'EWh', round(value/3600/1000*".$unitprice.",3) as 'totdol', round(sdur/60,1) as 'sdurmn', carid, status from energySession where carid=1 order by id desc";
 
-$result = $conn->query($sql);
+$sql_sommaire = "select year(from_unixtime(tstamp)) as 'yr',dayofyear(from_unixtime(tstamp)) as 'doy', date_format(from_unixtime(tstamp),'%M %e') as 'day', count(*) as 'nbs',round(sum(value)/3600/1000,2) as 'kwh', round(sum(sdur)/60,1) as 'sdur' from energySession where carid=1 group by yr,doy order by tstamp desc";
 
 echo <<<END
 <!DOCTYPE html>
@@ -22,33 +22,32 @@ echo <<<END
 <head>
 	<title>Liste des recharges de la Chevrolet Spark</title>
 	<meta charset="UTF-8" />
-	<style>
-	#customers {
-    font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
-    border-collapse: collapse;
-    width: 80%;
-}
-#customers td, #customers th {
-    border: 1px solid #ddd;
-    padding: 8px;
-}
-#customers tr:nth-child(even){background-color: #f2f2f2;}
-#customers tr:hover {background-color: #ddd;}
-#customers th {
-    padding-top: 12px;
-    padding-bottom: 12px;
-    text-align: left;
-    background-color: #4CAF50;
-    color: white;
-}
-</style>
+	<link rel="stylesheet" type="text/css" href="style2.css">
 </head>
 <body>
-	<h1>Liste des récentes recharges</h1><p>
+<h1>Recharges de la Chevrolet Spark</h1>
+<hr/>
+<h2>Sommaire par jour</h2>
 END;
 
+$result = $conn->query($sql_sommaire);
+echo "<table id=\"customers2\"><tr><th>Année</th><th>Mois / Jour</th><th>Nb sessions</th><th>Energie (kWh)</th><th>Durée (mn)</th></tr>";
 if ($result->num_rows > 0) {
-    echo "<table id=\"customers\"><tr><th>ID</th><th>Date Heure (fin)</th><th>Energie (Ws)</th><th>Energie (Wh)</th><th>Cout ($)</th><th>Durée (mn)</th><th>Vehicule ID</th><th>Status</th></tr>";
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>".$row["yr"]."</td><td>".$row["day"]."</td><td>".$row["nbs"]."</td><td>".$row["kwh"]."</td><td>".$row["sdur"]."</td></tr>";
+    }
+} else {
+    echo "<tr><td colspan='5'>Aucun résultat</td></tr>";
+}
+echo "</table>";
+
+$result = $conn->query($sql);
+
+echo "<hr/><h2>Liste des récentes recharges</h2>";
+
+echo "<table id=\"customers\"><tr><th>ID</th><th>Date Heure (fin)</th><th>Energie (Ws)</th><th>Energie (Wh)</th><th>Cout ($)</th><th>Durée (mn)</th><th>Vehicule ID</th><th>Status</th></tr>";
+
+if ($result->num_rows > 0) {
     $nb=0;
     $totEWs=0;
     $totEWh=0;
@@ -64,10 +63,11 @@ if ($result->num_rows > 0) {
         echo "<tr><td>".$row["id"]."</td><td>".$row["DateHeure"]."</td><td>".$row["EWs"]."</td><td>".$row["EWh"]."</td><td>".$row["totdol"]."</td><td>".$row["sdurmn"]."</td><td>".$row["carid"]."</td><td>".$row["status"]."</td></tr>";
     }
     echo "<tr><th>".$nb."</th><th></th><th>".$totEWs."</th><th>".$totEWh."</th><th>".$tottotdol."</th><th>".$totdurmn."</th><th></th><th></th></tr>";
-    echo "</table>";
 } else {
-    echo "<p>0 results</p>";
+    echo "<tr><td colspan='8'>Aucun résultat</td></tr>";
 }
+echo "</table>";
+
 echo "</body></html>";
 $conn->close();
 ?>
