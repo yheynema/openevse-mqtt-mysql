a1de819e1d590ed5aabfc4885730cc012574426c
Yanick H <yheynema@gmail.com>
2020-03-01 22:58:27 -0500

    Update openEVSE_MQTT_catcher_git.py
    Now logging to python logger handler

--- openEVSE_MQTT_catcher_git.py
+++ openEVSE_MQTT_catcher_git.py
@@ -16,15 +16,22 @@
 # La raison du merge est que le premier utilise MySQL pour logger mais pas la librairie Paho,
 # qui semble mieux standardisee que mosquitto. Le second n'utilise pas MySQL mais avec Paho.
 #
+# 1er mars 2020: ajout du logger
+
 import time
 import json
 import paho.mqtt.client as mqtt
 import mysql.connector
 from mysql.connector import errorcode
+import glob
+import logging
+import logging.handlers
 
-M_VERSION = "2.2.4"
+LOG_FILENAME = 'OpenEVSE-logger.out'
 
-M_INFO = 2                 # Print messages: 0 - none, 1 - Startup, 2 - Serial, 3 - All
+M_VERSION = "2.3.1"
+
+M_INFO = 2                 # Print messages: 0 - none, 1 - Error/Critial, 2 - status Info, 3 - Debug
 
 
 # MQTT Settings
@@ -35,9 +42,9 @@ MQTT_Topics = [("openevse/state", 0), ("openevse/wh", 0)]
 
 # mysql config
 sql_cfg = {
-    'user': 'username',
-    'password': 'password',
-    'host': 'AA.BB.CC.EE',
+    'user': 'DB_USER',
+    'password': 'DB_PASSWD',
+    'host': 'localhost',
     'database': 'mqtt',
     'raise_on_warnings': True
 }
@@ -67,7 +74,8 @@ state2Timestamp = 0
 
 def my_info(type, message):
     if M_INFO >= type:
-        print(message)
+#        print(message)
+         logger.info(message);
 
 # Subscribe to all Sensors at Base Topic
 
@@ -75,7 +83,7 @@ def my_info(type, message):
 def on_connect(mosq, obj, rc):
     with mqttc:
         mqttc.subscribe(MQTT_Topic)
-        my_info(1,"rc: "+str(rc))
+        logger.debug("rc: "+str(rc))
 
 
 def on_message(mosq, obj, msg):
@@ -88,13 +96,13 @@ def on_message(mosq, obj, msg):
     global initialWsReading
     global state2Timestamp
 
-    my_info(3,"On "+time.ctime()+":"+str(msg.topic) +
+    logger.debug("On "+time.ctime()+":"+str(msg.topic) +
           " "+str(msg.qos)+" "+str(msg.payload))
     if msg.topic == 'openevse/state':
         currentState = int(msg.payload)
-        my_info(4,"CurrSt="+str(currentState)+" lastState="+str(lastState))
+        logger.debug("CurrSt="+str(currentState)+" lastState="+str(lastState))
         if currentState != lastState:
-            my_info(2,'On '+time.ctime()+' - State changed: last='+str(lastState) +
+            logger.info('On '+time.ctime()+' - State changed: last='+str(lastState) +
                   " new="+str(currentState))
             # Dectecting state transition from (0,1) to 2, connected, a new session opens:
             if lastState <= 1 and currentState == 2:
@@ -112,23 +120,23 @@ def on_message(mosq, obj, msg):
                     logEnergyState = 1
                 else:
                     logEnergyState = 2
-                my_info(2,'Session started at: '+str(sessionStartAt))
+                logger.info('Session started at: '+str(sessionStartAt))
             # Decting end of charge session, moving from 3 down to 1 or 2:
             if currentState < lastState and currentState < 4 and currentState > 0:
                 if lastState == 3:
                     logEnergyState = lastState
                     sessionEndedAt = time.time()
-                    my_info(2,'Session ended at: '+str(sessionEndedAt))
+                    logger.info('Session ended at: '+str(sessionEndedAt))
             lastState = currentState
     if msg.topic == 'openevse/wh':
         # Read data in Watt-Second:
         currentWsReading = long(float(msg.payload))
-        my_info(4,'logEnergyState status: '+str(logEnergyState))
+        logger.debug('logEnergyState status: '+str(logEnergyState))
         # This session state went from disconnect to charging, thus a NEW session detected:
         if logEnergyState == 1:
             initialWsReading = 0
             logEnergyState = 0
-            my_info(2,'New session detected.')
+            logger.info('New session detected.')
         # This session state went from connected to charging, additional charge while being connected
         # In other words, this is a Delta session. The previousWsReading handling is important as when
         # the state transition, we can get the Ws reading 30 sec later, some consumption might not
@@ -141,27 +149,25 @@ def on_message(mosq, obj, msg):
               #This is better, normal, consider the last reading, just before transition from state 2 to 3:
               initialWsReading = previousWsReading
             logEnergyState = 0
-            my_info(2,'Delta session detected. Using initialWsReading as:'+str(initialWsReading))
+            logger.info('Delta session detected. Using initialWsReading as:'+str(initialWsReading))
         # End of session detected moved from charging down to either connected or disconnected, let's log the session details:
         if logEnergyState == 3:
             if currentWsReading > 0:
                 sessionTime = int(sessionEndedAt - sessionStartAt)
                 energySession = int(currentWsReading - initialWsReading)
-                my_info(2,'Session duration: '+str(sessionTime))
-                my_info(2,'Session Energy: '+str(energySession))
+                logger.info('Session duration: '+str(sessionTime))
+                logger.info('Session Energy: '+str(energySession))
                 if energySession > 0:
                     try:
-                        my_info(3,'Logging data to MySQL')
+                        logger.debug('Logging data to MySQL')
                         cnx = mysql.connector.connect(**sql_cfg)
                         cursor = cnx.cursor()
                         queryText = ("INSERT INTO energySession "
                             "(tstamp,value,sdur,carid,status) "
                             "VALUES (%s, %s, %s, %s, %s)")
-
-                        queryArgs = (time.time(), energySession,
-                            sessionTime, 1, 0)
+                        queryArgs = (time.time(), energySession,sessionTime, 1, 0)
                         cursor.execute(queryText, queryArgs)
-                        my_info(2,'Successfully Added record to mysql')
+                        logger.info('Successfully Added record to mysql')
                         # Make sure data is committed to the database
                         cnx.commit()
                         cursor.close()
@@ -169,17 +175,17 @@ def on_message(mosq, obj, msg):
                         logEnergyState = 0
                     except mysql.connector.Error as err:
                         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                          my_info(1,"Something is wrong with your user name or password")
+                          logger.critical("Something is wrong with your user name or password")
                         elif err.errno == errorcode.ER_BAD_DB_ERROR:
-                          my_info(1,"Database does not exist")
+                          logger.critical("Database does not exist")
                         else:
-                          my_inf(1,err)
-                        my_info(1, "Error encountered with database.")
+                          logger.error(err)
+                        logger.error("Error encountered with database.")
                     my_info(3,'Did the job...')
                 else:
-                    #Un-usual case, log everything to level 1 and continue:
-                    my_info(1,'PROBLEM: negative energy value. initialWsReading='+str(initialWsReading) + '; previousWsReading=' + str(previousWsReading) + '; currentWsReading='+str(currentWsReading)+'; logEnergyState='+str(logEnergyState)+'; sessionEndedAt='+str(sessionEndedAt)+'; state2Timestamp='+str(state2Timestamp)+'; sessionStartAt='+str(sessionStartAt)+';')
-                    logEnergyState = 0
+                  #Un-usual case, log everything to level 1 and continue:
+                  logger.error('PROBLEM: negative energy value. initialWsReading='+str(initialWsReading) + '; previousWsReading=' + str(previousWsReading) + '; currentWsReading='+str(currentWsReading)+'; logEnergyState='+str(logEnergyState)+'; sessionEndedAt='+str(sessionEndedAt)+'; state2Timestamp='+str(state2Timestamp)+'; sessionStartAt='+str(sessionStartAt)+';')
+                  logEnergyState = 0
         previousWsReading = currentWsReading
 
 def on_publish(mosq, obj, mid):
@@ -187,7 +193,7 @@ def on_publish(mosq, obj, mid):
 
 
 def on_subscribe(mosq, obj, mid, granted_qos):
-    my_info(2,"Subscribed: "+str(mid)+" "+str(granted_qos))
+    logger.info("Subscribed: "+str(mid)+" "+str(granted_qos))
 
 
 def on_log(mosq, obj, level, string):
@@ -196,12 +202,24 @@ def on_log(mosq, obj, level, string):
 
 def on_disconnect(client, userdata, rc):
     if rc != 0:
-        my_info(1,"Unexpected disconnection rc="+str(rc))
+        logger.critical("Unexpected disconnection rc="+str(rc))
 
 
 # --- MAIN -----------------------------------------------------
 
-my_info(1,'Starting script - v'+str(M_VERSION)+' - M level is: '+str(M_INFO))
+logger = logging.getLogger('OpenEVSE_mqtt_parser')
+logger.setLevel(logging.INFO)
+
+# create file handler which logs even debug messages
+handler = logging.handlers.RotatingFileHandler(LOG_FILENAME, maxBytes=1048576, backupCount=10)
+
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
+
+logger.info('EVSE logger - v'+str(M_VERSION)+' - M level is: '+str(M_INFO))
 
 mainloop = 1
 while mainloop == 1:
@@ -219,7 +237,7 @@ while mainloop == 1:
             mqttc.subscribe(MQTT_Topics)
             rc = 0
         except:
-            my_info(1, "Warning: No broker found. Retry in one minute.")
+            logger.warn("Warning: No broker found. Retry in one minute.")
             time.sleep(60)
             pass
     
@@ -230,10 +248,10 @@ while mainloop == 1:
             rc = 1
             time.sleep(5)
 
-    my_info(1,"Warning: Connection error - Restarting.")
+    logger.warn("Warning: Connection error - Restarting.")
 
     if M_INFO > 2:
         mainloop = 0
 
 mqttc.disconnect()
-my_info(1, "Disconnected, done.")
+logger.critical("End of program - Disconnected, done.")
